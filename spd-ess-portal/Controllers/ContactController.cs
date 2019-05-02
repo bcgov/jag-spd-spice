e250b0cc389e3253963b7c55792b5f7ccf1a98a8
George Walker <george@walkersoftware.ca>
2019-05-02 13:27:49 -0700

    Portal skeleton

---
+++ spd-ess-portal/Controllers/ContactController.cs
@@ -0,0 +1,370 @@
+﻿using Gov.Jag.Spice.Interfaces;
+using Gov.Jag.Spice.Interfaces.Models;
+using Gov.Jag.Spice.Public.Authentication;
+using Gov.Jag.Spice.Public.Models;
+using Gov.Jag.Spice.Public.Utils;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gov.Jag.Spice.Public.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContactController : Controller
+    {
+        private readonly IConfiguration Configuration;
+        private readonly IDynamicsClient _dynamicsClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ILogger _logger;
+        private readonly IHostingEnvironment _env;
+        private readonly SharePointFileManager _sharePointFileManager;
+
+        public ContactController(SharePointFileManager sharePointFileManager, IConfiguration configuration, IDynamicsClient dynamicsClient, IHttpContextAccessor httpContextAccessor, ILoggerFactory loggerFactory, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            _dynamicsClient = dynamicsClient;
+            _httpContextAccessor = httpContextAccessor;
+            _logger = loggerFactory.CreateLogger(typeof(ContactController));
+            _env = env;
+            _sharePointFileManager = sharePointFileManager;
+        }
+
+
+        /// <summary>
+        /// Get a specific legal entity
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContact(string id)
+        {
+            ViewModels.Contact result = null;
+
+            if (!string.IsNullOrEmpty(id))
+            {
+                Guid contactId = Guid.Parse(id);
+                // query the Dynamics system to get the contact record.
+                MicrosoftDynamicsCRMcontact contact = await _dynamicsClient.GetContactById(contactId);
+
+                if (contact != null)
+                {
+                    result = contact.ToViewModel();
+                }
+                else
+                {
+                    return new NotFoundResult();
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+            return Json(result);
+        }
+
+
+        /// <summary>
+        /// Update a legal entity
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateContact([FromBody] ViewModels.Contact item, string id)
+        {
+            if (id != null && item.id != null && id != item.id)
+            {
+                return BadRequest();
+            }
+
+            // get the contact
+            Guid contactId = Guid.Parse(id);
+
+            MicrosoftDynamicsCRMcontact contact = await _dynamicsClient.GetContactById(contactId);
+            if (contact == null)
+            {
+                return new NotFoundResult();
+            }
+            MicrosoftDynamicsCRMcontact patchContact = new MicrosoftDynamicsCRMcontact();
+            patchContact.CopyValues(item);
+            try
+            {
+                await _dynamicsClient.Contacts.UpdateAsync(contactId.ToString(), patchContact);
+            }
+            catch (OdataerrorException odee)
+            {
+                _logger.LogError("Error updating contact");
+                _logger.LogError("Request:");
+                _logger.LogError(odee.Request.Content);
+                _logger.LogError("Response:");
+                _logger.LogError(odee.Response.Content);
+            }
+
+            contact = await _dynamicsClient.GetContactById(contactId);
+            return Json(contact.ToViewModel());
+        }
+
+        /// <summary>
+        /// Create a contact
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public async Task<IActionResult> CreateContact([FromBody] ViewModels.Contact item)
+        {
+
+            // get UserSettings from the session
+            string temp = _httpContextAccessor.HttpContext.Session.GetString("UserSettings");
+            UserSettings userSettings = JsonConvert.DeserializeObject<UserSettings>(temp);
+
+            // first check to see that a contact exists.
+            string contactSiteminderGuid = userSettings.SiteMinderGuid;
+            if (contactSiteminderGuid == null || contactSiteminderGuid.Length == 0)
+            {
+                _logger.LogError(LoggingEvents.Error, "No Contact Siteminder Guid exernal id");
+                throw new Exception("Error. No ContactSiteminderGuid exernal id");
+            }
+
+            // get the contact record.
+            MicrosoftDynamicsCRMcontact userContact = null;
+
+            // see if the contact exists.
+            try
+            {
+                userContact = _dynamicsClient.GetContactByExternalId(contactSiteminderGuid);
+                if (userContact != null)
+                {
+                    throw new Exception("Contact already Exists");
+                }
+            }
+            catch (OdataerrorException odee)
+            {
+                _logger.LogError(LoggingEvents.Error, "Error getting contact by Siteminder Guid.");
+                _logger.LogError("Request:");
+                _logger.LogError(odee.Request.Content);
+                _logger.LogError("Response:");
+                _logger.LogError(odee.Response.Content);
+                throw new OdataerrorException("Error getting contact by Siteminder Guid");
+            }
+
+            // create a new contact.
+            MicrosoftDynamicsCRMcontact contact = new MicrosoftDynamicsCRMcontact();
+            contact.CopyValues(item);
+            
+
+            if (userSettings.IsNewUserRegistration)
+            {
+                // get additional information from the service card headers.
+                contact.CopyHeaderValues( _httpContextAccessor );
+            }        
+
+           
+            try
+            {
+                contact = await _dynamicsClient.Contacts.CreateAsync(contact);
+            }
+            catch (OdataerrorException odee)
+            {
+                _logger.LogError("Error updating contact");
+                _logger.LogError("Request:");
+                _logger.LogError(odee.Request.Content);
+                _logger.LogError("Response:");
+                _logger.LogError(odee.Response.Content);
+            }
+
+            // if we have not yet authenticated, then this is the new record for the user.
+            if (userSettings.IsNewUserRegistration)
+            {
+                userSettings.ContactId = contact.Contactid.ToString();
+
+                // we can now authenticate.
+                if (userSettings.AuthenticatedUser == null)
+                {
+                    Models.User user = new Models.User();
+                    user.Active = true;
+                    user.ContactId = Guid.Parse(userSettings.ContactId);
+                    user.UserType = userSettings.UserType;
+                    user.SmUserId = userSettings.UserId;
+                    userSettings.AuthenticatedUser = user;
+                }
+
+                userSettings.IsNewUserRegistration = false;
+
+                string userSettingsString = JsonConvert.SerializeObject(userSettings);
+                _logger.LogDebug("userSettingsString --> " + userSettingsString);
+
+                // add the user to the session.
+                _httpContextAccessor.HttpContext.Session.SetString("UserSettings", userSettingsString);
+                _logger.LogDebug("user added to session. ");
+            }
+            else
+            {
+                _logger.LogError(LoggingEvents.Error, "Invalid user registration.");
+                throw new Exception("Invalid user registration.");
+            }
+
+            return Json(contact.ToViewModel());
+        }
+
+        /// <summary>
+        /// Create a contact
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        [HttpPost("worker")]
+        public async Task<IActionResult> CreateWorkerContact([FromBody] ViewModels.Contact item)
+        {
+            // get UserSettings from the session
+            string temp = _httpContextAccessor.HttpContext.Session.GetString("UserSettings");
+            UserSettings userSettings = JsonConvert.DeserializeObject<UserSettings>(temp);
+
+            // first check to see that we have the correct inputs.
+            string contactSiteminderGuid = userSettings.SiteMinderGuid;
+            if (contactSiteminderGuid == null || contactSiteminderGuid.Length == 0)
+            {
+                _logger.LogError(LoggingEvents.Error, "No Contact Siteminder Guid exernal id");
+                throw new Exception("Error. No ContactSiteminderGuid exernal id");
+            }
+
+            // get the contact record.
+            MicrosoftDynamicsCRMcontact userContact = null;
+
+            // see if the contact exists.
+            try
+            {
+                userContact = _dynamicsClient.GetContactByExternalId(contactSiteminderGuid);
+                if (userContact != null)
+                {
+                    throw new Exception("Contact already Exists");
+                }
+            }
+            catch (OdataerrorException odee)
+            {
+                _logger.LogError(LoggingEvents.Error, "Error getting contact by Siteminder Guid.");
+                _logger.LogError("Request:");
+                _logger.LogError(odee.Request.Content);
+                _logger.LogError("Response:");
+                _logger.LogError(odee.Response.Content);
+                throw new OdataerrorException("Error getting contact by Siteminder Guid");
+            }
+
+            // create a new contact.
+            MicrosoftDynamicsCRMcontact contact = new MicrosoftDynamicsCRMcontact();
+            
+
+            contact.CopyValues(item);
+            // set the type to Retail Worker.
+            contact.Customertypecode = 845280000;
+
+            //Default the country to Canada
+            if (string.IsNullOrEmpty(contact.Address1Country))
+            {
+                contact.Address1Country = "Canada";
+            }
+            if (string.IsNullOrEmpty(contact.Address2Country))
+            {
+                contact.Address2Country = "Canada";
+            }
+
+            if (userSettings.IsNewUserRegistration && !_env.IsDevelopment())
+            {
+                // get additional information from the service card headers.
+                contact.CopyValues(userSettings.NewContact);
+                              
+            }
+            
+            
+
+
+            // if we have not yet authenticated, then this is the new record for the user.
+            if (userSettings.IsNewUserRegistration)
+            {
+                userSettings.ContactId = contact.Contactid.ToString();
+
+                // we can now authenticate.
+                if (userSettings.AuthenticatedUser == null)
+                {
+                    Models.User user = new Models.User();
+                    user.Active = true;
+                    user.ContactId = Guid.Parse(userSettings.ContactId);
+                    user.UserType = userSettings.UserType;
+                    user.SmUserId = userSettings.UserId;
+                    userSettings.AuthenticatedUser = user;
+                }
+
+                userSettings.IsNewUserRegistration = false;
+
+                string userSettingsString = JsonConvert.SerializeObject(userSettings);
+                _logger.LogDebug("userSettingsString --> " + userSettingsString);
+
+                // add the user to the session.
+                _httpContextAccessor.HttpContext.Session.SetString("UserSettings", userSettingsString);
+                _logger.LogDebug("user added to session. ");
+            }
+            else
+            {
+                _logger.LogError(LoggingEvents.Error, "Invalid user registration.");
+                throw new Exception("Invalid user registration.");
+            }
+
+            return Json(contact.ToViewModel());
+        }
+
+
+
+        /// <summary>
+        /// Get a document location by reference
+        /// </summary>
+        /// <param name="relativeUrl"></param>
+        /// <returns></returns>
+        private string GetDocumentLocationReferenceByRelativeURL(string relativeUrl)
+        {
+            string result = null;
+            string sanitized = relativeUrl.Replace("'", "''");
+            // first see if one exists.
+            var locations = _dynamicsClient.Sharepointdocumentlocations.Get(filter: "relativeurl eq '" + sanitized + "'");
+            var location = locations.Value.FirstOrDefault();
+
+            if (location == null)
+            {
+                //get parent location 
+                var parentSite = _dynamicsClient.Sharepointsites.Get().Value.FirstOrDefault();
+                var parentSiteRef = _dynamicsClient.GetEntityURI("sharepointsites", parentSite.Sharepointsiteid);
+                MicrosoftDynamicsCRMsharepointdocumentlocation newRecord = new MicrosoftDynamicsCRMsharepointdocumentlocation()
+                {
+                    Relativeurl = relativeUrl,
+                    Name = "Worker Qualification",
+                    ParentSiteODataBind = parentSiteRef
+                };
+                // create a new document location.
+                try
+                {
+                    location = _dynamicsClient.Sharepointdocumentlocations.Create(newRecord);
+                }
+                catch (OdataerrorException odee)
+                {
+                    _logger.LogError("Error creating document location");
+                    _logger.LogError("Request:");
+                    _logger.LogError(odee.Request.Content);
+                    _logger.LogError("Response:");
+                    _logger.LogError(odee.Response.Content);
+                }
+            }
+
+            if (location != null)
+            {
+                result = location.Sharepointdocumentlocationid;
+            }
+
+            return result;
+        }
+    }
+}
