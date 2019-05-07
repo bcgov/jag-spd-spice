244228e1a11e72a5dc72e6ed314307d86a714047
Mike Lyttle <mikelyttle@gmail.com>
2019-05-07 16:11:15 -0700

    Add stubbed-out controller for screening requests

---
+++ spd-ess-portal/Controllers/ScreeningRequestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gov.Jag.Spice.Public.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gov.Jag.Spice.Public.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScreeningRequestController : ControllerBase
+    {
+        // POST: api/ScreeningRequest
+        [HttpPost]
+        public IActionResult Post([FromBody] ScreeningRequest screeningRequest)
+        {
+            // TODO
+            return new JsonResult("Success");
+        }
+    }
+}
