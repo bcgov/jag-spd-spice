fe21af5c7830d153a2b8b2a14c4e22f333761df8
Mike Lyttle <mikelyttle@gmail.com>
2019-05-09 14:59:40 -0700

    Add screening request review page

--- spd-ess-portal/ClientApp/src/app/app-routing.module.ts
+++ spd-ess-portal/ClientApp/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ScreeningRequestFormComponent } from './screening-request-form/screening-request-form.component';
+import { ScreeningRequestReviewComponent } from './screening-request-review/screening-request-review.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { CanDeactivateGuard } from './services/can-deactivate-guard.service';
 import { BCeidAuthGuard } from './services/bceid-auth-guard.service';
@@ -11,7 +12,11 @@ const routes: Routes = [
   {
     path: '',
     component: ScreeningRequestFormComponent
-  },    
+  },
+  {
+    path: 'review-submission',
+    component: ScreeningRequestReviewComponent
+  },
   { path: '**', component: NotFoundComponent }
 ];
 
