8a880a9accdd8348a12981c18ad26d4f038220b2
Mike Lyttle <mikelyttle@gmail.com>
2019-05-23 12:50:57 -0700

    Update SiteMinder authentication and user controller for SPICE

--- spd-ess-portal/Controllers/LogoutController.cs
+++ spd-ess-portal/Controllers/LogoutController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using Gov.Jag.Spice.Public.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
@@ -12,57 +10,17 @@ namespace Gov.Jag.Spice.Public.Controllers
     public class LogoutController : Controller
     {
         private readonly IConfiguration Configuration;
-        private readonly IHostingEnvironment _env;
-        private readonly SiteMinderAuthOptions _options = new SiteMinderAuthOptions();
 
         public LogoutController(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
-            _env = env;         
         }
 
         [HttpGet]
         [AllowAnonymous]
-        public ActionResult Logout(string path)
-        {            
-            // clear session
+        public ActionResult Logout()
+        {
             HttpContext.Session.Clear();
-            if (! _env.IsProduction()) // clear dev tokens
-            {
-                string temp = HttpContext.Request.Cookies[_options.DevAuthenticationTokenKey];
-                if (temp == null)
-                {
-                    temp = "";
-                }
-                // expire "dev" user cookie
-                Response.Cookies.Append(
-                    _options.DevAuthenticationTokenKey,
-                    temp,
-                    new CookieOptions
-                    {
-                        Path = "/",
-                        SameSite = SameSiteMode.None,
-                        Expires = DateTime.UtcNow.AddDays(-1)
-                    }
-                );
-                // expire "dev" user cookie
-                temp = HttpContext.Request.Cookies[_options.DevBCSCAuthenticationTokenKey];
-                if (temp == null)
-                {
-                    temp = "";
-                }
-                Response.Cookies.Append(
-                    _options.DevBCSCAuthenticationTokenKey,
-                    temp,
-                    new CookieOptions
-                    {
-                        Path = "/",
-                        SameSite = SameSiteMode.None,
-                        Expires = DateTime.UtcNow.AddDays(-1)
-                    }
-                );
-
-            }
 
             string logoutPath = string.IsNullOrEmpty(Configuration["SITEMINDER_LOGOUT_URL"]) ? "/" : Configuration["SITEMINDER_LOGOUT_URL"];
             return Redirect(logoutPath + $"?returl={Configuration["BASE_URI"]}{Configuration["BASE_PATH"]}&retnow=1");
