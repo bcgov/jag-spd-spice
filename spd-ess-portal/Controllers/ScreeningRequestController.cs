98961164c623da1c5490bd9c845347115c3b7513
Mike Lyttle <mikelyttle@gmail.com>
2019-06-06 15:39:58 -0700

    Add server-side validation for client ministry and program area fields

--- spd-ess-portal/Controllers/ScreeningRequestController.cs
+++ spd-ess-portal/Controllers/ScreeningRequestController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
+using Gov.Jag.Spice.Public.Authentication;
 using Gov.Jag.Spice.Public.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -18,8 +20,32 @@ namespace Gov.Jag.Spice.Public.Controllers
 
         // POST: api/ScreeningRequest
         [HttpPost]
-        public IActionResult Post([FromBody] ScreeningRequest screeningRequest)
+        public async Task<IActionResult> Post([FromBody] ScreeningRequest screeningRequest, CancellationToken cancellationToken)
         {
+            // validate that the client ministry and program area specified in the screening request match the values provided by SiteMinder
+            var principal = HttpContext.User;
+            string siteMinderClientMinistry = principal.FindFirstValue(SiteMinderClaimTypes.COMPANY);
+            string siteMinderProgramArea = principal.FindFirstValue(SiteMinderClaimTypes.DEPARTMENT);
+
+            var ministries = await GetMinistryScreeningTypeData(cancellationToken);
+
+            var ministry = ministries.Find(m => m.Name == siteMinderClientMinistry);
+            if (ministry == null)
+            {
+                return Unauthorized();
+            }
+
+            var programArea = ministry.ProgramAreas.Find(a => a.Name == siteMinderProgramArea);
+            if (programArea == null)
+            {
+                return Unauthorized();
+            }
+
+            if (screeningRequest.ClientMinistry != ministry.Value || screeningRequest.ProgramArea != programArea.Value)
+            {
+                return BadRequest();
+            }
+
             // TODO
             return new JsonResult(new { requestId = 42 });
         }
@@ -29,8 +55,7 @@ namespace Gov.Jag.Spice.Public.Controllers
         [HttpGet]
         public async Task<IActionResult> GetMinistryScreeningTypes(CancellationToken cancellationToken)
         {
-            string text = await System.IO.File.ReadAllTextAsync(Path.Combine(_dataDirectory, "ministry-screening-types.json"), cancellationToken);
-            var data = JsonConvert.DeserializeObject<List<Ministry>>(text);
+            var data = await GetMinistryScreeningTypeData(cancellationToken);
             return new JsonResult(data);
         }
 
@@ -39,9 +64,20 @@ namespace Gov.Jag.Spice.Public.Controllers
         [HttpGet]
         public async Task<IActionResult> GetScreeningReasons(CancellationToken cancellationToken)
         {
-            string text = await System.IO.File.ReadAllTextAsync(Path.Combine(_dataDirectory, "screening-reasons.json"), cancellationToken);
-            var data = JsonConvert.DeserializeObject<List<string>>(text).Select(r => new ScreeningReason(r));
+            var data = await GetScreeningReasonData(cancellationToken);
             return new JsonResult(data);
+        }
+
+        private async Task<List<Ministry>> GetMinistryScreeningTypeData(CancellationToken cancellationToken)
+        {
+            string text = await System.IO.File.ReadAllTextAsync(Path.Combine(_dataDirectory, "ministry-screening-types.json"), cancellationToken);
+            return JsonConvert.DeserializeObject<List<Ministry>>(text);
+        }
+
+        private async Task<List<ScreeningReason>> GetScreeningReasonData(CancellationToken cancellationToken)
+        {
+            string text = await System.IO.File.ReadAllTextAsync(Path.Combine(_dataDirectory, "screening-reasons.json"), cancellationToken);
+            return JsonConvert.DeserializeObject<List<string>>(text).Select(r => new ScreeningReason(r)).ToList();
         }
     }
 }
