cd499d06c6641a0b9d38ea03087feacb8d031a70
Mike Lyttle <mikelyttle@gmail.com>
2019-06-19 11:35:14 -0700

    Switch logging library to Serilog

--- spd-ess-portal/Startup.cs
+++ spd-ess-portal/Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.HealthChecks;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.Net.Http.Headers;
 using Microsoft.Rest;
@@ -204,10 +203,8 @@ namespace Gov.Jag.Spice.Public
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            var logger = loggerFactory.CreateLogger<Startup>();
-
             string pathBase = Configuration["BASE_PATH"];
             if (string.IsNullOrEmpty(pathBase))
             {
