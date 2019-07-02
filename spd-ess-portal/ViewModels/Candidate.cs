85f2bbaab74aa9efcc30ead7e57d0352c74e4e1c
Mike Lyttle <mikelyttle@gmail.com>
2019-07-02 14:55:02 -0700

    Transfer data to and from Dynamics

---
+++ spd-ess-portal/ViewModels/Candidate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gov.Jag.Spice.Public.ViewModels
+{
+    public class Candidate
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+    }
+}
--- spd-ess-portal/ViewModels/ProgramArea.cs
+++ spd-ess-portal/ViewModels/ProgramArea.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Newtonsoft.Json;
 
 namespace Gov.Jag.Spice.Public.ViewModels
 {
@@ -10,13 +9,5 @@ namespace Gov.Jag.Spice.Public.ViewModels
         public string Name { get; set; }
         public string Value { get; set; }
         public List<ScreeningType> ScreeningTypes { get; set; }
-
-        [JsonConstructor]
-        public ProgramArea(string name, List<string> screeningTypes)
-        {
-            Name = name;
-            Value = name;
-            ScreeningTypes = screeningTypes.Select(t => new ScreeningType(t)).ToList();
-        }
     }
 }
