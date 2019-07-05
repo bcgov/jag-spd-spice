a6fdf1e41f52e705870d9c1fd801fe01b2c8aa23
Mike Lyttle <mikelyttle@gmail.com>
2019-07-05 12:09:27 -0700

    Improve logging in FileController

--- spd-ess-portal/Controllers/FileController.cs
+++ spd-ess-portal/Controllers/FileController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,19 +20,15 @@ namespace Gov.Jag.Spice.Public.Controllers
         private const string ScreeningDocumentUrlTitle = "incident";
 
         private readonly ILogger<FileController> _logger;
-        private readonly IConfiguration Configuration;
         private readonly FileManager _sharePointFileManager;
         private readonly IDynamicsClient _dynamicsClient;
 
-        public FileController(ILogger<FileController> logger, IConfiguration configuration, IDynamicsClient dynamicsClient, FileManager fileManager)
+        public FileController(ILogger<FileController> logger, IDynamicsClient dynamicsClient, FileManager fileManager)
         {
             _logger = logger;
-            Configuration = configuration;
             _sharePointFileManager = fileManager;
             _dynamicsClient = dynamicsClient;
         }
-
-
 
         private static string GetScreeningFolderName(MicrosoftDynamicsCRMincident screening)
         {
@@ -48,84 +42,83 @@ namespace Gov.Jag.Spice.Public.Controllers
         [DisableRequestSizeLimit]
         public async Task<IActionResult> UploadFile([FromRoute] string screeningId, [FromForm] IFormFile file)
         {
-            if (_sharePointFileManager == null)
-            {
-                _logger.LogWarning("Cannot upload file {FileName} for screening {ScreeningId} because no connection to SharePoint exists", file.Name, screeningId);
-                return new JsonResult(file.FileName);
-            }
-
             if (string.IsNullOrEmpty(screeningId))
             {
                 _logger.LogWarning("Cannot upload file without a screeningId");
                 return BadRequest();
             }
 
-            await CreateDocumentLibraryIfMissing(ScreeningDocumentListTitle, ScreeningDocumentUrlTitle);
-
-            bool hasAccess = await CanAccessScreening(screeningId);
-            if (!hasAccess)
-            {
-                _logger.LogWarning("Cannot access screening {ScreeningId}", screeningId);
-                return new NotFoundResult();
-            }
-
             try
             {
-                // Update modifiedon to current time
+                bool documentLibraryExists = await _sharePointFileManager.DocumentLibraryExists(ScreeningDocumentListTitle);
+                if (!documentLibraryExists)
+                {
+                    await _sharePointFileManager.CreateDocumentLibrary(ScreeningDocumentListTitle, ScreeningDocumentUrlTitle);
+                    _logger.LogInformation("Successfully created document library {ScreeningDocumentListTitle} on SharePoint", ScreeningDocumentListTitle);
+                }
+                else
+                {
+                    _logger.LogInformation("Successfully retrieved document library {ScreeningDocumentListTitle} on SharePoint", ScreeningDocumentListTitle);
+                }
+
+                // update screening modification time to current time
                 await UpdateScreeningModifiedOnDate(screeningId);
-            }
-            catch (OdataerrorException ex)
-            {
-                _logger.LogError(ex, string.Join(Environment.NewLine, "Failed to update last modification date for screening {ScreeningId}", "Request: {Request}", "Response: {Response}"), screeningId, ex.Request.Content, ex.Response.Content);
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
+                _logger.LogInformation("Successfully updated last modification date for screening {ScreeningId}", screeningId);
 
-            // Sanitize file name
-            var illegalInFileName = new Regex(@"[#%*<>?{}~¿""]");
-            string fileName = illegalInFileName.Replace(file.FileName, "");
-            illegalInFileName = new Regex(@"[&:/\\|]");
-            fileName = illegalInFileName.Replace(fileName, "-");
+                // retrieve folder name
+                var screening = await _dynamicsClient.GetScreeningById(Guid.Parse(screeningId));
+                string folderName = GetScreeningFolderName(screening);
+                _logger.LogInformation("Successfully retrieved SharePoint folder name {FolderName} for screening {ScreeningId}", folderName, screeningId);
 
-            fileName = FileUtility.CombineNameDocumentType(fileName, "WebUpload");
-            string folderName = await GetFolderName(screeningId);
-            try
-            {
+                // format file name
+                string fileName = SanitizeFileName(file.FileName);
+                fileName = FileUtility.CombineNameDocumentType(fileName, "WebUpload");
+
+                // upload to SharePoint
                 await _sharePointFileManager.AddFile(ScreeningDocumentUrlTitle, folderName, fileName, file.OpenReadStream(), file.ContentType);
                 _logger.LogInformation("Successfully uploaded file {FileName} to folder {FolderName} on SharePoint", fileName, folderName);
+
+                return new JsonResult(fileName);
             }
             catch (SharePointRestException ex)
             {
-                _logger.LogError(ex, "Failed to upload file {FileName} to folder {FolderName} on SharePoint {ResponseContent}", ex.Response.Content);
+                _logger.LogError(ex,
+                    string.Join(Environment.NewLine, "Failed to upload file {FileName} for screening {ScreeningId}", "Request: {@Request}", "Response: {@Response}"),
+                    file.Name,
+                    screeningId,
+                    ex.Request,
+                    ex.Response);
+
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
-            return new JsonResult(fileName);
+            catch (OdataerrorException ex)
+            {
+                _logger.LogError(ex,
+                    string.Join(Environment.NewLine, "Failed to upload file {FileName} for screening {ScreeningId}", "{@ErrorBody}"),
+                    file.Name,
+                    screeningId,
+                    ex.Body);
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to upload file {FileName} for screening {ScreeningId}", file.Name, screeningId);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
-        private async Task<bool> CanAccessScreening(string screeningId)
+        private static string SanitizeFileName(string fileName)
         {
-            var screening = await _dynamicsClient.GetScreeningById(Guid.Parse(screeningId));
-            return screening != null;
-        }
-
-        private async Task<string> GetFolderName(string screeningId)
-        {
-            var screening = await _dynamicsClient.GetScreeningById(Guid.Parse(screeningId));
-            return GetScreeningFolderName(screening);
+            fileName = new Regex(@"[#%*<>?{}~¿""]").Replace(fileName, "");
+            fileName = new Regex(@"[&:/\\|]").Replace(fileName, "-");
+            return fileName;
         }
 
         private async Task UpdateScreeningModifiedOnDate(string screeningId)
         {
             var patchScreening = new MicrosoftDynamicsCRMincident();
             await _dynamicsClient.Incidents.UpdateAsync(screeningId, patchScreening);
-        }
-
-        private async Task CreateDocumentLibraryIfMissing(string listTitle, string folderName)
-        {
-            bool exists = await _sharePointFileManager.DocumentLibraryExists(listTitle);
-            if (!exists)
-            {
-                await _sharePointFileManager.CreateDocumentLibrary(listTitle, folderName);
-            }
         }
     }
 }
