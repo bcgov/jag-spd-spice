d8b5d55c2f328ebff533f2d5f3256948db794b05
Brian Richter <brianorwhatever@gmail.com>
2019-07-11 01:46:09 -0700

    inserting incidents into spice dynamics

---
+++ interfaces/Dynamics-Autorest/ModelExtensions/MicrosoftDynamicsCRMaccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Gov.Jag.Spice.Interfaces.Models
+{
+    public partial class MicrosoftDynamicsCRMaccount
+    {
+        [JsonProperty(PropertyName = "spice_CompanyId@odata.bind")]
+        public string SpiceCompanyIdOdataBind { get; set; }
+
+        [JsonProperty(PropertyName = "primarycontactid@odata.bind")]
+        public string PrimaryContactIdOdataBind { get; set; }
+    }
+}
---
+++ interfaces/Dynamics-Autorest/ModelExtensions/MicrosoftDynamicsCRMspiceAccountcaseassignment.cs
@@ -0,0 +1,13 @@
+﻿namespace Gov.Jag.Spice.Interfaces.Models
+{
+    using Newtonsoft.Json;
+
+    public partial class MicrosoftDynamicsCRMspiceAccountcaseassignment
+    {
+        [JsonProperty(PropertyName = "spice_ContactCaseAssignmentId@odata.bind")]
+        public string SpiceContactCaseAssignmentIdODataBind { get; set; }
+
+        [JsonProperty(PropertyName = "spice_BusinessId@odata.bind")]
+        public string SpiceBusinessIdODataBind { get; set; }
+    }
+}
