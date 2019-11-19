6e1e289b2f48763ca9ec6fe3a33010c57f0eef45
Brian Richter <brianorwhatever@gmail.com>
2019-11-19 13:12:14 -0800

    Fixed dynamics client for incident resolution

--- interfaces/Dynamics-Autorest/Models/MicrosoftDynamicsCRMCloseIncidentParameters.cs
+++ interfaces/Dynamics-Autorest/Models/MicrosoftDynamicsCRMCloseIncidentParameters.cs
@@ -43,7 +43,8 @@ namespace Gov.Jag.Spice.Interfaces.Models
 
         /// <summary>
         /// </summary>
-        [JsonProperty(PropertyName = "status")]
+        [JsonProperty(PropertyName = "Status")]
+        // Had to change the case of the propertyname from status to Status to work after 9.0.8 upgrade
         public int? Status { get; set; }
 
     }
