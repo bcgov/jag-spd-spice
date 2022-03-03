76fa138ca877190631d386ce447e38e72fa8d389
Brendan Beach <55457785+BrendanBeachBC@users.noreply.github.com>
2022-03-03 15:57:27 -0800

    Upgraded to Angular 11

--- spd-ess-portal/ClientApp/src/app/screening-request-confirmation/screening-request-confirmation.component.spec.ts
+++ spd-ess-portal/ClientApp/src/app/screening-request-confirmation/screening-request-confirmation.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ScreeningRequestConfirmationComponent } from './screening-request-confirmation.component';
 
@@ -6,7 +6,7 @@ describe('ScreeningRequestConfirmationComponent', () => {
   let component: ScreeningRequestConfirmationComponent;
   let fixture: ComponentFixture<ScreeningRequestConfirmationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ScreeningRequestConfirmationComponent ]
     })
