8f78f626cc3679777b540a292e521bf02a3604d8
Chris Tihor <Christopher.Tihor@quartech.com>
2024-07-25 14:31:45 -0700

    SPDCSS-1141 Ignore: Sharepoint file upload fedauth cookie issue on OpenShift DEV env
    Made an automated test to check SharePoint service connectivity. Also fixed issue with running Angular CLI server in Development mode.

---
+++ interfaces/SharePoint/SharePointHealthCheck.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Gov.Jag.Spice.Interfaces.SharePoint
+{
+    public class SharePointHealthCheck : IHealthCheck
+    {
+        private readonly IConfiguration _configuration;
+
+        public SharePointHealthCheck(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default(CancellationToken))
+        {
+            FileManager sharePoint = new FileManager(_configuration);
+            // Try and get the Account document library
+            bool healthCheckResultHealthy;
+            try
+            {
+                healthCheckResultHealthy = sharePoint.TestStatus().GetAwaiter().GetResult();
+            }
+            catch (Exception)
+            {
+                healthCheckResultHealthy = false;
+            }
+
+            if (healthCheckResultHealthy)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("SharePoint is healthy."));
+            }
+            else
+            {
+                return Task.FromResult(HealthCheckResult.Unhealthy("SharePoint is unhealthy."));
+            }
+        }
+    }
+}
