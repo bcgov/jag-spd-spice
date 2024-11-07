0a983276019b0775973515b085892aedf6272b5f
Sam Warren <samwarren4@gmail.com>
2024-11-07 15:11:44 -0700

    SPDCSS-1192: Updates for stability

--- spd-ess-portal/ClientApp/src/app/models/outage-information.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export interface OutageInformation {
-    isOutage?: boolean;
-    content?: string | null;
-    outageEndDate?: string | null;
-    outageStartDate?: string | null;
-  }
-  
